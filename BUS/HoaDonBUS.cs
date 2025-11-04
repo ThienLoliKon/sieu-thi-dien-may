1bd3dfca64362f788d0c1dad6f31c686255cb2bc
ThienLoliKon <23211TT4279@mail.tdc.edu.vn>
2025-11-04 22:29:09 +0700

    update bus

---
+++ BUS/HoaDonBUS.cs
@@ -0,0 +1,87 @@
+﻿using DLL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+	public class HoaDonBUS
+	{
+		private HoaDonDLL dal;
+
+		public HoaDonBUS()
+		{
+			dal = new HoaDonDLL();
+		}
+
+		public bool AddHoaDon(string maNhanVienLap, string maKhachHang)
+		{
+			hoa_don addVariable = new hoa_don();
+			addVariable.ma_hoa_don = dal.TaoMaHoaDon();
+			addVariable.ma_nhan_vien_lap = maNhanVienLap;
+			addVariable.ma_khach_hang = maKhachHang;
+			addVariable.ngay_lap = DateTime.Now;
+			dal.addHoaDon(addVariable);
+			if (dal.check(addVariable.ma_hoa_don) == true) { return false; }
+			return true;
+		}
+
+	
+
+		public bool UpdateHoaDon(string maHoaDon, string maNhanVienLap, string maKhachHang)
+		{
+			hoa_don updateItem = new hoa_don();
+
+			updateItem.ma_nhan_vien_lap = maNhanVienLap;
+
+			updateItem.ma_khach_hang = maKhachHang;
+			dal.updateHoaDon(updateItem);
+
+			if (dal.check(updateItem.ma_hoa_don) == true) { return false; }
+			return true;
+		}
+
+		public DataTable timHoaDon(string keyword)
+		{
+			List<hoa_don> listData = dal.searchByNameOrID(keyword);
+			if (listData == null || listData.Count == 0)
+			{
+				return null;
+			}
+			DataTable dt = new DataTable();
+			dt.Columns.Add("ma_hoa_don", typeof(string));
+			dt.Columns.Add("ma_nhan_vien_lap", typeof(float));
+			dt.Columns.Add("ma_khach_hang", typeof(string));
+			dt.Columns.Add("ngay_lap", typeof(DateTime));
+			foreach (var indexData in listData)
+			{
+				dt.Rows.Add(indexData.ma_hoa_don, indexData.ma_nhan_vien_lap, indexData.ma_khach_hang, indexData.ngay_lap);
+			}
+
+			return dt;
+		}
+
+		public DataTable GetAllHoaDonAsTable()
+		{
+			List<hoa_don> listData = dal.GetAllHoaDon(); // Kiểm tra danh sách từ DAL
+			if (listData == null || listData.Count == 0)
+			{
+				return null;
+			}
+			DataTable dt = new DataTable();
+			dt.Columns.Add("ma_hoa_don", typeof(string));
+			dt.Columns.Add("ma_nhan_vien_lap", typeof(float));
+			dt.Columns.Add("ma_khach_hang", typeof(string));
+			dt.Columns.Add("ngay_lap", typeof(DateTime));
+			foreach (var indexData in listData)
+			{
+				dt.Rows.Add(indexData.ma_hoa_don, indexData.ma_nhan_vien_lap, indexData.ma_khach_hang, indexData.ngay_lap);
+			}
+			return dt;
+		}
+
+	}
+}
---
+++ BUS/SanPhamLoaiHangBUS.cs
@@ -0,0 +1,98 @@
+﻿using DLL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+	public class SanPhamLoaiHangBUS
+	{
+		private SanPhamLoaiHangDLL dal;
+
+		public SanPhamLoaiHangBUS()
+		{
+			dal = new SanPhamLoaiHangDLL();
+		}
+
+		public bool AddSanPhamLoaiHang(string maSanPham,string maLoaiHang)
+		{
+			san_pham_loai_hang addVariable = new san_pham_loai_hang();
+			addVariable.ma_loai_hang = maLoaiHang;
+			addVariable.ma_san_pham = maSanPham;
+			dal.addSanPhamLoaiHang(addVariable);
+			if (dal.check(maSanPham, maLoaiHang) == true) { return false; }
+			return true;
+		}
+
+		public bool DeleteSanPhamLoaiHang(string maSanPham, string maLoaiHang)
+		{
+			dal.deleteSanPhamLoaiHang(maSanPham,maLoaiHang);
+			if (dal.check(maSanPham,maLoaiHang) == true) { return false; }
+			return true;
+		}
+
+		public bool UpdateSanPhamLoaiHang(string maSanPham, string maLoaiHang)
+		{
+			san_pham_loai_hang updateItem = new san_pham_loai_hang();
+			updateItem.ma_loai_hang = maLoaiHang;
+			updateItem.ma_san_pham = maLoaiHang;
+			dal.updateSanPhamLoaiHang(updateItem);
+			if (dal.check(updateItem.ma_san_pham,updateItem.ma_loai_hang) == true) { return false; }
+			return true;
+		}
+
+		public DataTable timSanPhamLoaiHangTheoSanPham(string keyword)
+		{
+			List<san_pham_loai_hang> listData = dal.searchByNameOrIDLoaiHang(keyword);
+			if (listData == null || listData.Count == 0)
+			{
+				return null;
+			}
+			DataTable dt = new DataTable();
+			dt.Columns.Add("ma_san_pham", typeof(string));
+			dt.Columns.Add("ma_loai_hang", typeof(float));
+			foreach (var indexData in listData)
+			{
+				dt.Rows.Add(indexData.ma_san_pham, indexData.ma_loai_hang);
+			}
+
+			return dt;
+		}
+		public DataTable timSanPhamLoaiHangTheoLoaiHang(string keyword)
+		{
+			List<san_pham_loai_hang> listData = dal.searchByNameOrIDLoaiHang(keyword);
+			if (listData == null || listData.Count == 0)
+			{
+				return null;
+			}
+			DataTable dt = new DataTable();
+			dt.Columns.Add("ma_san_pham", typeof(string));
+			dt.Columns.Add("ma_loai_hang", typeof(float));
+			foreach (var indexData in listData)
+			{
+				dt.Rows.Add(indexData.ma_san_pham, indexData.ma_loai_hang);
+			}
+
+			return dt;
+		}
+		public DataTable GetAllLoaiHang()
+		{
+			List<san_pham_loai_hang> listData = dal.GetAllSanPhamLoaiHang(); // Kiểm tra danh sách từ DAL
+			if (listData == null || listData.Count == 0)
+			{
+				return null;
+			}
+			DataTable dt = new DataTable();
+			dt.Columns.Add("ma_san_pham", typeof(string));
+			dt.Columns.Add("ma_loai_hang", typeof(float));
+			foreach (var indexData in listData)
+			{
+				dt.Rows.Add(indexData.ma_san_pham, indexData.ma_loai_hang);
+			}
+			return dt;
+		}
+	}
+}
